d478d0573c79095e318715ad7dcb791a687e9501
Raghuraamm <Raghusony2005@gmail.com>
2026-02-06 03:48:58 +0530

    [CI] fix ABI3 issues

--- setup.py
+++ setup.py
@@ -15,8 +15,22 @@ try:
 except ImportError:
     BinaryDistribution = None
 
+try:
+    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+
+    class bdist_wheel(_bdist_wheel):
+        def get_tag(self):
+            python, abi, plat = super().get_tag()
+            # Since we use ABI3, we are compatible with any Python 3.x
+            if python.startswith("cp"):
+                return "py3", "none", plat
+            return python, abi, plat
+except ImportError:
+    bdist_wheel = None
+
 setup(
     distclass=BinaryDistribution,
+    cmdclass={"bdist_wheel": bdist_wheel} if bdist_wheel else {},
     package_data={
         "pytron": ["dependencies/*", "dependencies/**/*", "installer/*", "manifests/*"],
     },
