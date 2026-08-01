d8c0dacaf655d83dc8ef048385ce8f54e6be5feb
Ghua8088 <raghusony2005@gmail.com>
2026-08-01 19:54:45 +0530

    [feat] include default app_id in settings.json generated by pytron init

--- pytron/commands/init.py
+++ pytron/commands/init.py
@@ -138,7 +138,9 @@ def cmd_init(args: argparse.Namespace) -> int:
         "title": target.name,
         "version": "1.0.0",
         "author": "Your Name",
+        "app_id": f"com.yourname.{target.name.lower().replace('-', '_')}",
         "description": "A brief description of your app",
+
         "copyright": f"Copyright © 2026 Your Name",
         "pytron_version": __version__,
         # Window Configuration
